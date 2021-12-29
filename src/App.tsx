41b05e00d14a7a921d83e419ddbbac2c635df975
Robson Formigão <robson.formigao@scaffoldeducation.com.br>
2021-12-28 23:12:36 -0300

    feat(app): add dependencies and create styles global

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
-      <h1>Hellow World</h1>
+      <h1>Hello World</h1>
+
+      <GlobalStyle />
     </div>
   );
 }
